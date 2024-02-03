4d9798928a6105ed9a5bd8d4a541aef4df189de6
dinhtkpc06914 <132972173+dinhtkpc06914@users.noreply.github.com>
2024-02-03 17:52:51 +0700

    cc

--- Lab7/app.js
+++ Lab7/app.js
@@ -1,7 +1,6 @@
 let string2 = 'hello';
-let string3 =     'hello';
+let string3 = 'hello';
            let string4 =    'hello';
-
-      let stringFinal = "Xin Chao"+string2+string3+string4;
+ let stringFinal = "X         in Chao"+string2+string3+string4;
 
                                       stringFinal;
