76ee3d8b74d93dcf86eea76f5d99f11d6a9c70d6
dinhtkpc06914 <132972173+dinhtkpc06914@users.noreply.github.com>
2024-02-03 17:07:57 +0700

    lab7

--- Lab7/app.js
+++ Lab7/app.js
@@ -3,7 +3,7 @@ const name = 'Đình';
 const person = { first: name }; // Thêm khoảng trắng sau dấu hai chấm
 console.log(person);
 
-const sayHelloLinting = (fName) => {
+const   sayHelloLinting = (fName) => {
     console.log(`Hello Linting, ${fName}`);
 };
 
